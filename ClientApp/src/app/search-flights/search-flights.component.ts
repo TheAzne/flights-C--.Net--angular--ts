6230740e9af4d8287c8518e359fe2320849e2557
azne <a_kurbanov@hotmail.com>
2023-06-23 21:26:10 +0200

    updating with some data and created interface

--- ClientApp/src/app/search-flights/search-flights.component.ts
+++ ClientApp/src/app/search-flights/search-flights.component.ts
@@ -7,10 +7,45 @@ import { Component } from '@angular/core';
 })
 export class SearchFlightsComponent {
 
-  searchResult: any =[
-    "American Airlines",
-    "British Airways",
-    "Lufthansa"
+  searchResult: FlightRm[] =[
+    {
+      airline: "American Airlines",
+      remainingNumberOfSeats: 500,
+      departure: {time:Date.now().toString(), place:"Los Angeles"},
+      arrival: {time:Date.now().toString(), place:"Istanbul"},
+      price:"350",
+    },
+
+    {
+      airline: "Deutsche BA",
+      remainingNumberOfSeats: 60,
+      departure: {time:Date.now().toString(), place:"Munchen"},
+      arrival: {time:Date.now().toString(), place:"Schipol"},
+      price:"600",
+    },
+    {
+      airline: "SAS",
+      remainingNumberOfSeats: 300,
+      departure: {time:Date.now().toString(), place:"Stockholm"},
+      arrival: {time:Date.now().toString(), place:"Gran Canaria"},
+      price:"400",
+    }
+
   ]
+
+}
+
+
+export interface FlightRm{
+  airline:string;
+  arrival: TimePlaceRm;
+  departure: TimePlaceRm;
+  price:string;
+  remainingNumberOfSeats:number;
+}
+
+export interface TimePlaceRm{
+  place: string;
+  time: string;
 
 }
