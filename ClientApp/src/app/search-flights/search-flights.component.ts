01386a495d06e06812ae5bd6b313445dd69c5aac
azne <a_kurbanov@hotmail.com>
2023-06-23 20:54:38 +0200

    first commit

---
+++ ClientApp/src/app/search-flights/search-flights.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-flights',
+  templateUrl: './search-flights.component.html',
+  styleUrls: ['./search-flights.component.css']
+})
+export class SearchFlightsComponent {
+
+  searchResult: any =[
+    "American Airlines",
+    "British Airways",
+    "Lufthansa"
+  ]
+
+}
