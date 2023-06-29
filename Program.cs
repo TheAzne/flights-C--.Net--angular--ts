6f52cd27a485f8ed9cb561b41e930b7bbb471c53
azne <a_kurbanov@hotmail.com>
2023-06-29 14:37:06 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen(c=>{
+    c.AddServer(new OpenApiServer{
+        Description = "Development Server",
+        Url = "Https://localhost:7177"
+    });
+});
 
 var app = builder.Build();
+
+app.UseCors(builder=> builder.WithOrigins("*"));
+app.UseSwagger().UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
